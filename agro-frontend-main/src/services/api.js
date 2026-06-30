ffedb8737a5adb73bd4608fb5347f9fa5d59b32f
iamatultiwwari <atult2386@gmail.com>
2026-07-01 02:25:39 +0530

    upadated the api for connecting backend to frontend

--- agro-frontend-main/src/services/api.js
+++ agro-frontend-main/src/services/api.js
@@ -1,26 +1,7 @@
-// import axios from "axios";
-
-// const API = axios.create({
-//   baseURL: "http://localhost:5000/api/v1",
-// });
-
-// API.interceptors.request.use((req) => {
-//   const token = localStorage.getItem("token");
-
-//   if (token) {
-//     req.headers.Authorization = `Bearer ${token}`;
-//   }
-
-//   return req;
-// });
-
-// export default API;
-
 import axios from "axios";
 
 const API = axios.create({
-  // Remove /v1 so it cleanly matches your backend route layout
-  baseURL: "http://localhost:5000/api", 
+  baseURL: "https://your-backend-service-name.onrender.com/api", 
 });
 
 API.interceptors.request.use((req) => {
