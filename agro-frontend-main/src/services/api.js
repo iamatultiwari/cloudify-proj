588baba946e97b3218defd46174895beb1d848f6
iamatultiwwari <atult2386@gmail.com>
2026-07-01 03:17:24 +0530

    upadated the api

--- agro-frontend-main/src/services/api.js
+++ agro-frontend-main/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://your-backend-service-name.onrender.com/api", 
+  baseURL: "https://cloudify-proj-backend.onrender.com", 
 });
 
 API.interceptors.request.use((req) => {
