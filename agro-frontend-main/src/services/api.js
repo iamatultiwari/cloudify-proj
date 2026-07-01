17b3d50bce952c836a98c0c63b29eadfc960f13d
iamatultiwwari <atult2386@gmail.com>
2026-07-01 11:34:22 +0530

    upadated endpoint of backend

--- agro-frontend-main/src/services/api.js
+++ agro-frontend-main/src/services/api.js
@@ -1,16 +1,15 @@
 import axios from "axios";
 
+// Add /api to the end of your base URL
 const API = axios.create({
-  baseURL: "https://cloudify-proj-backend.onrender.com", 
+  baseURL: "https://cloudify-proj-backend.onrender.com/api", 
 });
 
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
-
   if (token) {
     req.headers.Authorization = `Bearer ${token}`;
   }
-
   return req;
 });
 
